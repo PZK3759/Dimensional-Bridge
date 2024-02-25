bc17b623074f9e27952c2f87c1c2b15056633bef
Abdullah Al Zahin <118601852+PZK3759@users.noreply.github.com>
2024-02-25 16:34:51 +0600

    added more global variables in Global.dart, added textfields and buttons in OverToNether.dart, updated pubspec.yaml

--- lib/Global.dart
+++ lib/Global.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
 
-Color primaryColor = Color(0xff56186F);
+Color primaryColor = Color(0xff56186F),
+    secondaryColor = Color(0xff6F2A18),
+    activeTFColor = Color(0xff56186F),
+    inactiveTFColor = Color(0xff6F2A18),
+    primaryButtonColor = Color(0xff56186F);
--- lib/Screens/OverToNether.dart
+++ lib/Screens/OverToNether.dart
@@ -1,7 +1,11 @@
+import 'package:dimensional_bridge/Global.dart';
 import 'package:dimensional_bridge/Widgets/AppbarWidget.dart';
+import 'package:dimensional_bridge/Widgets/CustomButton.dart';
+import 'package:dimensional_bridge/Widgets/CustomTextField.dart';
 import 'package:dimensional_bridge/Widgets/NavigationDrawerWidget.dart';
 import 'package:flutter/material.dart';
 import 'package:dimensional_bridge/Widgets/AppbarWidget.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class OverToNether extends StatefulWidget {
   const OverToNether({super.key});
@@ -11,6 +15,24 @@ class OverToNether extends StatefulWidget {
 }
 
 class _OverToNetherState extends State<OverToNether> {
+
+  TextEditingController overWorld_X_Controller = TextEditingController();
+  TextEditingController overWorld_Y_Controller = TextEditingController();
+  TextEditingController overWorld_Z_Controller = TextEditingController();
+
+  TextEditingController nether_X_Controller = TextEditingController();
+  TextEditingController nether_Y_Controller = TextEditingController();
+  TextEditingController nether_Z_Controller = TextEditingController();
+
+
+  String overWorld_X = "";
+  String overWorld_Y = "";
+  String overWorld_Z = "";
+
+  String nether_X = "";
+  String nether_Y = "";
+  String nether_Z = "";
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(child: Scaffold(
@@ -23,13 +45,65 @@ class _OverToNetherState extends State<OverToNether> {
             Row(children: [
               Text("Overworld Coordinates", style: TextStyle(fontSize: 18),),
               Expanded(child: Container()),
-              IconButton(onPressed: (){
-                //todo: implement fucntion
-              }, icon: Icon(Icons.change_circle_outlined)),
+              IconButton(onPressed: clearTextFields,
+                  icon: Icon(Icons.clear_outlined)),
             ]),
+            CustomTextField(overWorld_X_Controller, Text("X"), false),
+            SizedBox(height: 10,),
+            CustomTextField(overWorld_Y_Controller, Text("Y"), false),
+            SizedBox(height: 10,),
+            CustomTextField(overWorld_Z_Controller, Text("Z"), false),
+            SizedBox(height: 50,),
+            SizedBox(
+              width: double.infinity,
+              child: CustomButton(getNetherCoordinates),
+            ),
+            SizedBox(height: 50,),
+            Row(children: [
+              Text("Nether Coordinates", style: TextStyle(fontSize: 18),),
+              Expanded(child: Container()),
+            ]),
+            SizedBox(height: 15,),
+            CustomTextField(nether_X_Controller, Text("X"), true),
+            SizedBox(height: 10,),
+            CustomTextField(nether_Y_Controller, Text("Y"), true),
+            SizedBox(height: 10,),
+            CustomTextField(nether_Z_Controller, Text("Z"), true),
           ],
         ),
       ),
     ));
   }
+
+
+  void clearTextFields(){
+    overWorld_X_Controller.text = "";
+    overWorld_Y_Controller.text = "";
+    overWorld_Z_Controller.text = "";
+
+    nether_X_Controller.text = "";
+    nether_Y_Controller.text = "";
+    nether_Z_Controller.text = "";
+  }
+
+  void getNetherCoordinates(){
+    overWorld_X = overWorld_X_Controller.text;
+    overWorld_Y = overWorld_Y_Controller.text;
+    overWorld_Z = overWorld_Z_Controller.text;
+
+    nether_X = (int.parse(overWorld_X)/8).truncate().toString();
+    nether_Y = overWorld_Y;
+    nether_Z = (int.parse(overWorld_Z)/8).truncate().toString();
+
+    nether_X_Controller.text = nether_X;
+    nether_Y_Controller.text = nether_Y;
+    nether_Z_Controller.text = nether_Z;
+
+    setState(() {
+
+    });
+
+  }
+
+
 }
